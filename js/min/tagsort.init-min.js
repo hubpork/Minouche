58d65a1640fb7025ae3ff560db7174e3a888589b
Michel Maillard <michel@porkhead.org>
2018-08-05 14:49:15 +0200

    Project Start
    Start

---
+++ js/min/tagsort.init-min.js
@@ -0,0 +1,1 @@
+$(function(){$(".tags-container").tagSort({items:".item",tagElement:"span",tagClassPrefix:"tagsort-",itemTagsView:".item-tags",itemTagsSeperator:" / ",sortType:"single",fadeTime:2}),$(".tags-container").find(".tagsort-all").addClass("tagsort-active")});
